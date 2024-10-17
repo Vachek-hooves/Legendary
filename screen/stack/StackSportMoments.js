bf97efad8c7ff61a48f4c7ce8c0a52a051709f27
Vachek-hooves <vachek0602@gmail.com>
2024-10-17 16:55:41 +0300

    moments dataset

---
+++ screen/stack/StackSportMoments.js
@@ -0,0 +1,14 @@
+import { StyleSheet, Text, View } from 'react-native'
+import React from 'react'
+
+const StackSportMoments = () => {
+  return (
+    <View>
+      <Text>StackSportMoments</Text>
+    </View>
+  )
+}
+
+export default StackSportMoments
+
+const styles = StyleSheet.create({})
