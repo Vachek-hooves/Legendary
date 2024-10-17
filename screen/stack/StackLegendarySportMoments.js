cbeed64c5a21d46f237691026b00ca49a369f474
Vachek-hooves <vachek0602@gmail.com>
2024-10-17 18:47:35 +0300

    create moments data

---
+++ screen/stack/StackLegendarySportMoments.js
@@ -0,0 +1,14 @@
+import { StyleSheet, Text, View } from 'react-native'
+import React from 'react'
+
+const StackLegendarySportMoments = () => {
+  return (
+    <View>
+      <Text>StackLegendarySportMoments</Text>
+    </View>
+  )
+}
+
+export default StackLegendarySportMoments
+
+const styles = StyleSheet.create({})
--- screen/stack/index.js
+++ screen/stack/index.js
@@ -3,5 +3,12 @@ import StackQuizLevelScreen from './StackQuizLevelScreen';
 import StackTrainingDetailScreen from './StackTrainingDetailScreen';
 import StackTrainingProgramScreen from './StackTrainingProgramScreen';
 import WelcomeScreen from './WelcomeScreen';
-
-export { StackFootballPlay, StackQuizLevelScreen, StackTrainingDetailScreen, StackTrainingProgramScreen, WelcomeScreen };
+import StackLegendarySportMoments from './StackLegendarySportMoments';
+export {
+  StackFootballPlay,
+  StackQuizLevelScreen,
+  StackTrainingDetailScreen,
+  StackTrainingProgramScreen,
+  WelcomeScreen,
+  StackLegendarySportMoments,
+};
